62678c6befdde73b20ce7409b245f6920207889a
Gil <gilyavne@outlook.com>
2017-10-27 14:50:53 +0300

    =modified index page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+var title = "get your professional image today!"
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Yavgor' });
+  res.render('index', {title});
 });
 
 module.exports = router;
